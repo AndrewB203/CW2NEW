00dfdb93d90ab3b09baa98ca7085ba47a46f904a
AndrewB203 <bolshakov17419@gmail.com>
2024-05-19 14:37:59 +0300

    add CMake file

---
+++ build/CMakeFiles/FlyweightDesignPattern.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlyweightDesignPattern.dir/FlyweightPattern.cpp.o"
+  "CMakeFiles/FlyweightDesignPattern.dir/FlyweightPattern.cpp.o.d"
+  "FlyweightDesignPattern"
+  "FlyweightDesignPattern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlyweightDesignPattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/FlyweightDesignPattern.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FlyweightDesignPattern.
+# This may be replaced when dependencies are built.
